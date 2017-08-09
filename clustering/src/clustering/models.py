c8fa32b47b948420ad59b0bc15e920378e7ad811
Clare Hsu <clare.hsu390@gmail.com>
2017-08-08 19:01:06 -0700

    Add basic layout to index.html

--- clustering/src/clustering/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Iris(models.Model):
-    sepal_length = models.FloatField()
-    sepal_width = models.FloatField()
-    petal_length = models.FloatField()
-    petal_width = models.FloatField()
-    name = models.CharField()
