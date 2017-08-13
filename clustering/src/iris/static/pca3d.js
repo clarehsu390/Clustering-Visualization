c4c9426395b20dc5921e8122f920b94c7c793fa0
yiyi1026 <vivian.li.uibe@gmail.com>
2017-08-13 12:57:02 -0700

    style pca3d

--- clustering/src/iris/static/pca3d.js
+++ clustering/src/iris/static/pca3d.js
@@ -13,7 +13,7 @@ var X = d3.scaleLinear()
 var Y = d3.scaleLinear()
     .range([height, 0]);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#pca").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
